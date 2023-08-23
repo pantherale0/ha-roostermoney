ac3da59cb1314df18d4100f66cab248e61b14732
Jordan H <jordan@hrvy.uk>
2023-08-23 20:34:59 +0100

    0.3.4
    Enables card switch
    Adds switch to enable the allowance (and expose allowance settings)

--- custom_components/rooster_money/switch.py
+++ custom_components/rooster_money/switch.py
@@ -40,8 +40,56 @@ async def async_setup_entry(
                 entity_id="card",
             )
         )
+        entities.append(
+            RoosterAllowanceEntity(
+                coordinator=hass.data[DOMAIN][config_entry.entry_id],
+                idx=None,
+                child_id=child.user_id,
+                entity_id="allowance",
+            )
+        )
 
     async_add_entities(entities, True)
+
+
+class RoosterAllowanceEntity(RoosterChildEntity, SwitchEntity):
+    """A allowance switch that enables or disables the allowance."""
+
+    @property
+    def name(self) -> str:
+        return "Allowance"
+
+    @property
+    def unique_id(self) -> str:
+        return f"{self._child.first_name}_allowance"
+
+    @property
+    def is_on(self) -> bool:
+        return self._child.allowance
+
+    @property
+    def device_class(self) -> SwitchDeviceClass | None:
+        return SwitchDeviceClass.SWITCH
+
+    @property
+    def device_state_attributes(self) -> Mapping[str, Any] | None:
+        return {
+            "amount": self._child.allowance_amount,
+            "day": self._child.allowance_day,
+            "last_paid": self._child.allowance_last_paid,
+        }
+
+    async def async_turn_on(self, **kwargs: Any) -> None:
+        """Enable regular allowance."""
+        await self._child.update_allowance(
+            paused=False, amount=self._child.allowance_amount
+        )
+
+    async def async_turn_off(self, **kwargs: Any) -> None:
+        """Disable regular allowance."""
+        await self._child.update_allowance(
+            paused=True, amount=self._child.allowance_amount
+        )
 
 
 class RoosterCardEntity(RoosterChildEntity, SwitchEntity):
@@ -54,6 +102,10 @@ class RoosterCardEntity(RoosterChildEntity, SwitchEntity):
     @property
     def is_on(self) -> bool:
         return self._child.card.status == "active"
+
+    @property
+    def unique_id(self) -> str:
+        return f"{self._child.first_name}_card"
 
     @property
     def device_class(self) -> SwitchDeviceClass | None:
@@ -71,3 +123,11 @@ class RoosterCardEntity(RoosterChildEntity, SwitchEntity):
     @property
     def entity_picture(self) -> str | None:
         return self._child.card.image
+
+    async def async_turn_on(self, **kwargs: Any) -> None:
+        """Enable the card."""
+        await self._child.card.set_card_status(True)
+
+    async def async_turn_off(self, **kwargs: Any) -> None:
+        """Disable the card."""
+        await self._child.card.set_card_status(False)
